26d5c235beda3eec750378d4151067152fd574e2
oEdgarAlves <edgaralves@ua.pt>
2021-01-07 18:22:25 +0000

    kepperApp finished

--- src/components/CreateArea.jsx
+++ src/components/CreateArea.jsx
@@ -1,10 +1,14 @@
 import React, { useState } from "react";
+import Fab from '@material-ui/core/Fab';
+import Zoom from '@material-ui/core/Zoom';
 
 function CreateArea(props) {
   const [note, setNote] = useState({
     title: "",
     content: ""
   });
+
+  const[expanded, setExpanded] = useState(false);
 
   function handleChange(event) {
     const { name, value } = event.target;
@@ -26,23 +30,30 @@ function CreateArea(props) {
     event.preventDefault();
   }
 
+  function expandCreationArea(){
+    setExpanded(true);
+  }
+
   return (
     <div>
       <form className="create-note">
-        <input
+        {expanded && <input
           name="title"
           onChange={handleChange}
           value={note.title}
           placeholder="Title"
-        />
+        />}
         <textarea
           name="content"
           onChange={handleChange}
+          onClick={expandCreationArea}
           value={note.content}
           placeholder="Take a note..."
-          rows="3"
+          rows={expanded ? "3" : "1"}
         />
-        <button onClick={submitNote}>Add</button>
+        <Zoom in={expanded}>
+        <Fab onClick={submitNote}>Add</Fab>
+        </Zoom>
       </form>
     </div>
   );
