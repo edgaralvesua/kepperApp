75c0eb5f2784df343dc60e7efa7b0a508c0f760b
oEdgarAlves <edgaralves@ua.pt>
2021-01-07 15:50:09 +0000

    New Version

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,27 +1,40 @@
-import React,{useState} from "react";
+import React, { useState } from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
 import CreateArea from "./CreateArea";
 
 function App() {
-  const [notes,setNotes] = useState([]);
+  const [notes, setNotes] = useState([]);
 
-  function addNewNote(newNote){
+  function addNote(newNote) {
     setNotes(prevNotes => {
-      return [...prevNotes,newNote];
-    })
-  };
+      return [...prevNotes, newNote];
+    });
+  }
 
-  function deleteNote(id){
-    setNotes(prevNotes => prevNotes.filter((note,index) => index !== id ) )
+  function deleteNote(id) {
+    setNotes(prevNotes => {
+      return prevNotes.filter((noteItem, index) => {
+        return index !== id;
+      });
+    });
   }
 
   return (
     <div>
       <Header />
-      <CreateArea addNote={addNewNote}/>
-      {notes.map((note,index) => { return <Note key={index} index={index} title={note.title} content={note.content} delNote={deleteNote} />;
+      <CreateArea onAdd={addNote} />
+      {notes.map((noteItem, index) => {
+        return (
+          <Note
+            key={index}
+            id={index}
+            title={noteItem.title}
+            content={noteItem.content}
+            onDelete={deleteNote}
+          />
+        );
       })}
       <Footer />
     </div>
--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -1,17 +1,15 @@
 import React from "react";
 
 function Note(props) {
-
-  function deleteNote(e){
-    e.preventDefault();
-    props.delNote(props.index);
+  function handleClick() {
+    props.onDelete(props.id);
   }
 
   return (
     <div className="note">
       <h1>{props.title}</h1>
       <p>{props.content}</p>
-      <button onClick={deleteNote} className="noteButton" >DELETE</button>
+      <button onClick={handleClick}>DELETE</button>
     </div>
   );
 }
